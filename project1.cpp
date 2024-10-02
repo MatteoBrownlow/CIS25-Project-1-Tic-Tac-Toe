cb7836a3e2572cd7d7bc261bf70f80f1b37a6a14
MatteoBrownlow <114264680+MatteoBrownlow@users.noreply.github.com>
2024-10-02 12:05:10 -0700

    Update project1.cpp
    added instructions

--- project1.cpp
+++ project1.cpp
@@ -4,4 +4,13 @@
 #include <ctime>
 using namespace std;
 
-
+int main(){
+    int players = 0; 
+    char playAgain = 'y';
+    char array[3][3] = {{' ', ' ', ' '},{' ', ' ', ' '},{' ', ' ', ' '}};
+    cout << " 1 | 2 | 3 " << endl;
+	cout << "___________" << endl;
+	cout << " 4 | 5 | 6 " << endl;          //initializes the array, prints the instructions, and determines who goes first
+	cout << "___________" << endl;
+	cout << " 7 | 8 | 9 " << endl << endl;
+	cout << "Each box is numbered; enter the corresponding number to mark an 'O'" << endl;
