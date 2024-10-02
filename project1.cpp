eb0a27d03bdffb8e702b43c89bdb3c30989d6aa1
MatteoBrownlow <114264680+MatteoBrownlow@users.noreply.github.com>
2024-10-02 12:06:38 -0700

    Update project1.cpp
    added 2 player game

--- project1.cpp
+++ project1.cpp
@@ -14,3 +14,22 @@ int main(){
 	cout << "___________" << endl;
 	cout << " 7 | 8 | 9 " << endl << endl;
 	cout << "Each box is numbered; enter the corresponding number to mark an 'O'" << endl;
+    while(players != 1 && players!= 2){
+        cout << "How many players? Enter 1 or 2: " << endl;
+        cin >> players;
+    }
+    if(players == 2){
+        while(playAgain == 'y'){    //if there are two players, this loops the twoPlayerGame function until they don't want to play anymore
+        twoPlayerGame(array);
+        cout << "The score is " << playerOneWins << " to " << playerOneLosses << ", with " << playerOneDraws << " draw";
+        if(playerOneDraws !=1){
+            cout << "s";
+        }
+        cout << "." << endl;
+        eraseArray(array);
+        do{
+            cout << "Play again? (y/n)" << endl;
+            cin >> playAgain;
+        } while(playAgain != 'y' && playAgain != 'n');
+        }
+    }
