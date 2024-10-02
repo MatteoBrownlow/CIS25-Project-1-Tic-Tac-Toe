4f95779a8c531245a31a1881b99c45fe164681c7
MatteoBrownlow <114264680+MatteoBrownlow@users.noreply.github.com>
2024-10-02 12:08:19 -0700

    Update project1.cpp
    Added full game for the computer going first

--- project1.cpp
+++ project1.cpp
@@ -63,3 +63,110 @@ int main(){
 
     
 }
+
+int computerGoesFirst(char array[3][3]){
+    cout << "The computer will go first" << endl;
+    sleep(1);
+    array[0][0] = 'X';  //the computer always has the same first move
+    printArray(array);
+    
+    yourMove(array); 
+    cout << "Your move:" << endl;
+    printArray(array);  //gets then displays the player's first move
+
+    sleep(1);
+        
+        if(array[1][0] == 'O' || array[2][0] == 'O' || array[1][2] == 'O'){
+            array[0][2] = 'X';
+        }  
+        else{
+            array[2][0] = 'X';
+        }
+    cout << "Computer move:" << endl;   //the computer's second move
+    printArray(array);
+                
+    yourMove(array); 
+    cout << "Your move:" << endl;
+    printArray(array);  //the player's second move
+    
+    sleep(1);
+    if(array[2][0] == 'X' && array[1][0] == ' '){
+        array[1][0] = 'X';
+        printArray(array);
+        cout << "The machine wins! Humanity's downfall is imminent." << endl;
+        computerWins++;
+
+        return 0;           //if the computer wins, then the output should be different and the game should end on the spot
+    }
+    else if(array[0][2] == 'X' && array[0][1] == ' '){
+        array[0][1] = 'X';
+        printArray(array);
+        cout << "The machine wins! Humanity's downfall is imminent." << endl;
+        computerWins++;
+        return 0;
+    }
+    else if(array[1][0] == 'O' && array[1][1] == 'O'){
+        array[1][2] = 'X';
+    }
+    else if(array [0][1] == 'O' && (array[1][0] == 'O' || array[1][2]) && array[1][1] == ' '){
+        array[1][1] = 'X';
+    }
+    else if((array[2][0] == 'X') && (array[0][1] == ' ' && array[1][1] == ' ') && array[0][2] == ' '){
+        array[0][2] = 'X';
+    }
+    else{                       //these if/else statements print the best move by checking which squares are occupied with which letters
+        array[2][2]= 'X';
+    }
+    cout << "Computer move:" << endl;
+    printArray(array);
+          
+    yourMove(array); //the player's third move
+    cout << "Your move:" << endl;
+    printArray(array);
+    
+    sleep(1);
+    if(checkWins(array)){
+        cout << "Computer move:" << endl;
+        printArray(array);
+        cout << "The machine won! Humanity's downfall is imminent." << endl;
+        computerWins++;
+        return 0;
+    }
+    else if(array[0][1] == 'O' || array[0][2] == 'O'){
+        array[2][1] = 'X';
+    }
+    else{
+        array[0][1] = 'X';
+    }
+    cout << "Computer move:" << endl;   //the computer's fourth move
+    printArray(array);
+        
+    yourMove(array); //the player's fourth move
+    cout << "Your move:" << endl;
+    printArray(array);
+
+    sleep(1);
+    //the computer's fifth move is the last of the game, so there will only be one square left
+    //this means we can use nested for loops to find the first (and only) open square
+    if(checkWins(array)){
+        cout << "Computer move:" << endl;
+        printArray(array);
+        cout << "The machine wins! Humanity's downfall is imminent." << endl;
+        computerWins++;
+        return 0;
+    }
+    else{
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                if(array[i][j] == ' '){
+                    array[i][j] = 'X';
+                }
+            }
+        }
+    }
+    cout << "Computer move:" << endl;
+    printArray(array);
+    cout << "It's a draw." << endl;
+    computerDraws++;
+    return 0;
+}
